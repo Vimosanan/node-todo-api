be004f5bf002bc22cdb8176f9f0bca262396b72a
Vimosanan <vimosanan.13@cse.mrt.ac.lk>
2018-05-09 18:15:53 +0530

    server updated

---
+++ server/server.js
@@ -0,0 +1,63 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var {moongose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req,res) => {
+  var todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save().then((doc) => {
+    res.send(doc);
+  },(e) => {
+    res.status(400).send(e);
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server started on 3000');
+});
+
+
+// var newTodo = new Todo({
+//   text: 'You eat',
+//   completed: true,
+//   completedAt: 4
+// });
+//
+// newTodo.save().then((doc) => {
+//   console.log('Sucessfully Saved.', doc);
+// }, (e) => {
+//   console.log('Unable to save.');
+// });
+//
+//
+// var otherTodo = new Todo({
+//   text: "   Have to eat   ",
+//   completed: true,
+//   completedAt: 15
+// });
+//
+// var user = new User({
+//   email: 'andrew@yahoo.com'
+// });
+//
+// user.save().then((doc) => {
+//   console.log('Sucessfully saved.', doc );
+// }, (err) => {
+//   console.log('Unable to save.')
+// })
+//
+// otherTodo.save().then((doc) => {
+//   console.log('Sucessfully Saved.', doc);
+// }, (e) => [
+//   console.log('Unable to save.')
+// ])
