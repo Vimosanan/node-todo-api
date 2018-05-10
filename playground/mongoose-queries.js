df57d87edf79465bf3aac470d607068892e13b84
Vimosanan <vimosanan.13@cse.mrt.ac.lk>
2018-05-10 14:43:31 +0530

    mongoose queries added

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,38 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+var id='5af2c648d804662698b902f6';
+
+if(!ObjectID.isValid(id)){
+  console.log('ID is invalid');
+}
+
+// Todo.find({
+//   _id: id
+// }).then((todos) => {
+//   console.log('Todos', todos);
+// });
+//
+//
+// Todo.findOne({
+//   _id:id
+// }).then((todo) => {
+//   console.log('Todo', todo);
+// });
+
+// Todo.findById(id).then((todo) => {
+//   if(!todo){
+//     return console.log('ID not found.')
+//   }
+//   console.log('Todo by ID', todo);
+// }).catch((e) => console.log(e));
+
+User.findById(id).then((user) => {
+  if(!user){
+    return console.log(`User with id: ${id} not found`)
+  }
+  console.log(JSON.stringify(user, undefined, 2));
+}).catch((e) => console.log(e));
