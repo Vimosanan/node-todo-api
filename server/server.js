02ce508f20bbf50534d2d0e3dc5a9aa9619d25d3
Vimosanan <vimosanan.13@cse.mrt.ac.lk>
2018-05-10 16:18:25 +0530

    GET todo/id modified

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+var {ObjectID} = require('mongodb');
 var {moongose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
@@ -28,6 +29,23 @@ app.get('/todos', (req,res) => {
     }, (e) => {
       res.status(400).send(e);
     });
+});
+
+app.get('/todos/:id', (req, res) =>{
+  var id = req.params.id;
+
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+  
+  Todo.findById(id).then((todos) => {
+    if(!todos){
+      return res.status(404).send();
+    }
+    res.send({todos});
+  }).catch((e) => {
+    res.status(400).send(e);
+  });
 });
 
 app.listen(3000, () => {
